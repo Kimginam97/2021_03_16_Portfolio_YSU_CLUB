e3de2333bc91c7682ab5927811bae00874905925
LEESANGNAM <fgh0116@naver.com>
2021-04-28 22:27:10 +0900

    동아리삭제시 멤버상태변경

--- src/main/java/kr/ac/yeonsung/demo/repository/JoinClubRepository.java
+++ src/main/java/kr/ac/yeonsung/demo/repository/JoinClubRepository.java
@@ -2,29 +2,25 @@ package kr.ac.yeonsung.demo.repository;
 
 import kr.ac.yeonsung.demo.domain.Join;
 import kr.ac.yeonsung.demo.domain.JoinClub;
+import kr.ac.yeonsung.demo.domain.JoinclubMapping;
+import kr.ac.yeonsung.demo.domain.club.Club;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class JoinClubRepository {
+public interface JoinClubRepository extends JpaRepository<JoinClub,Long> {
+    List<JoinclubMapping> findByClub(Club club);
+    //클럽삭제할떄 클럽 id 가져와서 디비 쿼리들가는게 id 하나만찾게
+    // 해당되는 클럽 id 가지고있는 신청현황 id만 list로 가져옴
+    // 가져온거 반복문으로 돌려삭제
+    // 아니다 돌랴야게따 while롣 돌리샘
 
-    private final EntityManager em;
-    private JoinClub joinClub;
-
-    public void save(JoinClub joinClub){
-        em.persist(joinClub);
-    }
-
-    //동아리 조회
-    public JoinClub findOne(Long id){
-        return em.find(JoinClub.class,id);
-    }
-
-    public void deleteOne(JoinClub joinClub) {
-        em.remove(joinClub);
-    }
 }
 
